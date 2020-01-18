de1f4c2de34b63d77b1267478d7d8a9638cdbfa2
Matthew Burruss <burrussmatthew@gmail.com>
2020-01-18 17:04:31 -0600

    ready to go

--- src/InceptionV3_Attack.py
+++ src/InceptionV3_Attack.py
@@ -9,6 +9,7 @@ import os
 from keras.models import load_model
 from art.attacks import FastGradientMethod,DeepFool,CarliniL2Method,BasicIterativeMethod,ProjectedGradientDescent
 from art.utils import load_mnist
+from art.metrics import empirical_robustness
 import innvestigate
 import innvestigate.utils
 import numpy as np
@@ -22,12 +23,13 @@ from keras.models import Model
 from keras import regularizers
 from keras.regularizers import l2
 from matplotlib import pyplot as plt
+from ModifiedKerasClassifier import KerasClassifier as DefaultKerasClassifier
 
 from models.InceptionV3Model import InceptionV3Model
 from AdversarialAttacks import CarliniWagnerAttack,ProjectedGradientDescentAttack,FGSMAttack,DeepFoolAttack,BasicIterativeMethodAttack
 from AdversarialAttacks import HistogramOfPredictionConfidence,ConfusionMatrix
 from keras.applications.inception_v3 import preprocess_input
-
+import numpy.linalg as la
 # os.environ["CUDA_VISIBLE_DEVICES"]="1" # second gpu
 # os.environ["CUDA_VISIBLE_DEVICES"]="2" # second gpu
 # os.environ["CUDA_VISIBLE_DEVICES"]="3" # second gpu
@@ -44,6 +46,39 @@ baseDir = "/content/drive/My Drive/Colab Notebooks/InceptionV3Weights"
 imagenet_baseDir = './vgg16_dataset_10_partitioned'
 attackBaseDir="/content/drive/My Drive/Colab Notebooks/AdversaryAttacks"
 #'/media/scope/99e21975-0750-47a1-a665-b2522e4753a6/weights/AdversaryAttacks/
+
+
+def calc_l2normperturbation(model,xadv,x_clean,y_clean):
+        y_pred = model.predict(xadv)
+        idxs = (np.argmax(y_pred, axis=1) != np.argmax(y_clean, axis=1))
+        if np.sum(idxs) == 0.0:
+            print('No incorrect predictions!')
+            return 0
+        norm_type = 2
+        perts_norm = la.norm((xadv - x_clean).reshape(x_clean.shape[0], -1), ord=norm_type, axis=1)
+        perts_norm = perts_norm[idxs]
+        robust= np.mean(perts_norm / la.norm(x_clean[idxs].reshape(np.sum(idxs), -1), ord=norm_type, axis=1))
+        return robust
+
+def calc_empirical_robustness(model,X):
+    classifier = DefaultKerasClassifier(defences=[],model=model, use_logits=False)
+    robust = empirical_robustness(classifier,X)
+    return robust
+
+def calc_true_and_false_positive(model,xadv,x_clean,y_clean):
+    predictions_adv = model.predict(xadv)
+    predictions_clean = model.predict(x_clean)
+    adv_labels = np.argmax(predictions_adv,axis=1)
+    clean_labels = np.argmax(y_clean,axis=1)
+    pred_labels = np.argmax(predictions_clean,axis=1)
+    true_positive_idx = np.where(np.logical_and(adv_labels != clean_labels,pred_labels==clean_labels))[0]
+    true_positive_rate = len(true_positive_idx) / len(clean_labels)
+    incorrect_label_idx = adv_labels[true_positive_idx]
+    incorrect_predictions = predictions_adv[true_positive_idx]
+    TP_Mean_Confidence = np.mean(incorrect_predictions[np.arange(incorrect_predictions.shape[0]),incorrect_label_idx])
+    false_positive_idx = np.where(np.logical_and(adv_labels != clean_labels,pred_labels!=clean_labels))[0]
+    false_positive_rate = len(false_positive_idx) / len(clean_labels)
+    return true_positive_rate,false_positive_rate,TP_Mean_Confidence
 
 def createAttack(x_test,y_test,anomaly_clean,softmax_clean):
     # Set attacks true or false
@@ -96,33 +131,54 @@ def createAttack(x_test,y_test,anomaly_clean,softmax_clean):
         y = np.load(os.path.join(attackBaseDir,'y_test_adv_orig.npy'))
         print('Exiting: Already designed adversary images.')
 
+    # empirical robustness
+    print('Calculating the empirical robustness of the two classifiers using entire test dataset: n=',len(x_test))
+    robust_rbf = calc_empirical_robustness(anomaly_clean.model,x_test)
+    robust_softmax = calc_empirical_robustness(softmax_clean.model,x_test)
+    print('Softmax:',robust_softmax)
+    print('RBF: ', robust_rbf)
+
     for attack in attacks:
         attackName = attack['name']
         title = attack['title']
         print('Evaluating Attack:',attackName)
         attack_function = attack['function']
-        print('Creating attack for softmax model...')
-        xadv = attack_function(model=softmax_clean.model,
+        print('Loading attack for softmax model...')
+        xadv_softmax = attack_function(model=softmax_clean.model,
             X=x,
             path=os.path.join(attackBaseDir,attackName,'softmax_clean_attack.npy'))
-        print('Softmax model on attack ', attackName,'...')
-        softmax_clean.evaluate(xadv,y)
-        P1 = softmax_clean.predict(xadv)
-        confidence = P1[np.arange(P1.shape[0]),np.argmax(P1,axis=1)]
-        print('Softmax average confidence, ', np.mean(confidence),'\n Softmax less than 0.5',np.sum(confidence<0.05)/len(confidence))
-        print('\n')
-
-        print('Creating attack for anomaly detector...')
-        xadv = attack_function(model=anomaly_clean.model,
+        print('Loading attack for rbf classifier...')
+        xadv_rbf = attack_function(model=anomaly_clean.model,
             X=x,
             path=os.path.join(attackBaseDir,attackName,'anomaly_clean_attack.npy'))
-        print('Anomaly Detector on attack ', attackName,'...')
-        anomaly_clean.evaluate(xadv,y)
-        P1 = anomaly_clean.predict(xadv)
-        confidence = P1[np.arange(P1.shape[0]),np.argmax(P1,axis=1)]
-        print('Anomaly average confidence, ', np.mean(confidence),'\n Anomaly less than 0.5',np.sum(confidence<0.05)/len(confidence))
-        print('\n')
-
+        print("#################################################333")
+        print('\nEvaluating TP and FP on softmax')
+        TP,FP,TP_Mean = calc_true_and_false_positive(softmax_clean,xadv_softmax,x,y)
+        print('TP:',TP)
+        print('FP:',FP)
+        print('Mean Confidence TP',TP_Mean)
+        print('\nEvaluating transferability TP and FP on softmax')
+        TP,FP,TP_Mean = calc_true_and_false_positive(softmax_clean,xadv_rbf,x,y)
+        print('Transferability TP:',TP)
+        print('Transferability FP:',FP)
+        print('Transferability Mean Confidence TP',TP_Mean)
+        print('\nEvaluating average l2 norm...')
+        l2_robust = calc_l2normperturbation(softmax_clean,xadv_softmax,x,y)
+        print('L2 perturbation normalized',l2_robust)
+        print("#################################################333")
+        print('\nEvaluating TP and FP on Anomaly')
+        TP,FP,TP_Mean = calc_true_and_false_positive(anomaly_clean,xadv_rbf,x,y)
+        print('TP:',TP)
+        print('FP:',FP)
+        print('Mean Confidence TP',TP_Mean)
+        print('\nEvaluating transferability TP and FP on softmax')
+        TP,FP,TP_Mean = calc_true_and_false_positive(anomaly_clean,xadv_softmax,x,y)
+        print('Transferability TP:',TP)
+        print('Transferability FP:',FP)
+        print('Transferability Mean Confidence TP',TP_Mean)
+        print('\nEvaluating average l2 norm...')
+        l2_robust = calc_l2normperturbation(anomaly_clean,xadv_rbf,x,y)
+        print('L2 perturbation normalized',l2_robust)
 
 
 def evaluateAttack(x_test,y_test,anomaly_clean,softmax_clean):
@@ -193,6 +249,7 @@ def evaluateAttack(x_test,y_test,anomaly_clean,softmax_clean):
         confidence = P1[np.arange(P1.shape[0]),np.argmax(P1,axis=1)]
         print('Softmax average confidence, ', np.mean(confidence),'\n Softmax less than 0.5',np.sum(confidence<0.05)/len(confidence))
         print('\n')
+        
         print('Creating attack for anomaly detector...')
         xadv = attack_function(model=anomaly_clean.model,
             X=x,
